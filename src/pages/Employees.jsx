3479ee1c4f7abcbf22693aded3033b1086c53e43
Evangel <dev.evangel@gmail.com>
2022-06-26 17:22:50 +0100

    Added project build files

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  GridComponent,
+  Page,
+  Inject,
+  Search,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
+
+const Employees = () => {
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        width="auto"
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Employees;
